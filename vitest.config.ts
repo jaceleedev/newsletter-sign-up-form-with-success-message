27e57b8a4e1e132ef5025e73194839f83cf96dc2
jaceleedev <jaceleedev@gmail.com>
2024-08-08 15:30:13 +0900

    build: :heavy_plus_sign: install vitest and testing dependencies

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+// vitest.config.ts
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+  },
+});
